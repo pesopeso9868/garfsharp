24f3e9046ac55869871be3fb19e0f1c98e4d6071
pesopeso9868 <pesopeso9868@outlook.com>
2020-09-16 15:57:09 -0400

    Garfield.exe will no longer have an aneurysm if you are missing strips.json or if it is locked

--- recompile.bat
+++ recompile.bat
@@ -1,2 +1,2 @@
 taskkill /im "Garfield.exe"
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\Roslyn\csc.exe" -debug -reference:Newtonsoft.Json.dll -out:Garfield.exe -target:winexe garfield.cs && garfield || pause
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\Roslyn\csc.exe" -debug:full -reference:Newtonsoft.Json.dll -out:Garfield.exe -target:winexe garfield.cs && garfield.exe || pause
