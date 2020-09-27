6bef6a256fdac9c752d509e828b8751148bf8d2f
pesopeso9868 <pesopeso9868@outlook.com>
2020-09-27 10:21:23 -0400

    .Github will be new workflows

--- recompile.bat
+++ recompile.bat
@@ -1,2 +1,2 @@
 taskkill /im "Garfield.exe"
-".\csc\csc.exe" -debug:full -reference:Newtonsoft.Json.dll -reference:System.Net.Http.dll -out:Garfield.exe -target:winexe garfield.cs && garfield.exe || pause
+".\csc\csc.exe" -debug:full -r:Newtonsoft.Json.dll -r:System.Net.Http.dll -out:Garfield.exe -target:winexe garfield.cs && garfield.exe || pause
